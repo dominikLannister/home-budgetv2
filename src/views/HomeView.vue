49c3225133d3944cdef80e40bd4951d53864dc3d
Dominik <dominik24aa@gmail.com>
2022-08-08 13:09:53 +0200

    Add Connection to Api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,33 @@
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios';
+
 import HelloWorld from '@/components/HelloWorld.vue';
+
+const API_APP = 'C:/Users/Dominik/Desktop/HomeBudgetV2/home-budgetv2/connect.php';
+
+// @ is an alias to /src
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld,
   },
+
+  methods: {
+    getUnits: function() {
+      axios.get('home-budgetv2/Api/connect.php')
+      .then((response) => {
+        console.log(response);
+    })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+},
+beforeMount(){
+   this.getUnits()
+},
 };
 </script>
